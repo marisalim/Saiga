e0515c9021e93e417c2fad1809d4b938f668cbda
Marisa Lim <marisalim@users.noreply.github.com>
2019-10-28 16:21:17 -0400

    dir setup

---
+++ setuppipe.py
@@ -0,0 +1,27 @@
+# create the directory structure
+
+import os
+
+toppath = os.getcwd() #this gets current working directory path
+
+print('---------------------------------------------------')
+print('Setting up output directories... ')
+
+commands = """
+echo 'Making directories'
+cd {0}
+if [ ! -d '{0}/1_basecalled' ]; then mkdir {0}/1_basecalled; fi
+if [ ! -d '{0}/2a_samp_lists' ]; then mkdir {0}/2a_samp_lists; fi
+if [ ! -d '{0}/2b_demultiplexed' ]; then mkdir {0}/2b_demultiplexed; fi
+if [ ! -d '{0}/3_readclustering' ]; then mkdir {0}/3_readclustering; fi
+if [ ! -d '{0}/4_consensus' ]; then mkdir {0}/4_consensus; fi
+if [ ! -d '{0}/Pipeline_logfiles' ]; then mkdir {0}/Pipeline_logfiles; fi
+""".format(toppath)
+
+command_list = commands.split('\n')
+for cmd in command_list:
+    os.system(cmd)
+
+print('Move guppy basecalled files to 1_basecalled/')
+print('Move sample input files to 2a_samp_lists/')
+print('---------------------------------------------------')
