c32a0b49d2541e4bb6e4424624c18ef851a17881
Marisa Lim <marisalim@users.noreply.github.com>
2019-10-29 16:24:40 -0400

    fixed spoa fasta header naming
    now you can see the cluster id from isonclust for cd-hit clstr output! woohoo!!!

--- Pipeline_scripts/spoafy.sh
+++ Pipeline_scripts/spoafy.sh
@@ -8,5 +8,6 @@ clstr_path=$1
 for file in $clstr_path/*.fastq; do echo $file; spoa $file > $file.spoa; done
 
 # rename file and fasta header
-for file in $clstr_path/*.spoa; do awk 'NR==1{print line">" $0 "|"FILENAME;}END{print $0" "}' $file > $(echo $file | cut -f1 -d.)_spoa.fasta; done
+for file in $clstr_path/*.spoa; do awk -v var=">clstr$(echo $file | rev | cut -f1 -d/ |rev |cut -f1 -d.)" 'NR==1{print var "|" $0;}END{print $0" "}' $file > $(echo $file | cut -f1 -d.)_spoa.fasta; done
+
 rm $clstr_path/*.fastq.spoa
