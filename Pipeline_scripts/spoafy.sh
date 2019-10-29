6f9ef4814d0f9fe849e4a99735dd12a9520b66b1
Marisa Lim <marisalim@users.noreply.github.com>
2019-10-29 14:28:11 -0400

    edited fasta header names

--- Pipeline_scripts/spoafy.sh
+++ Pipeline_scripts/spoafy.sh
@@ -7,5 +7,6 @@ clstr_path=$1
 # make consensus
 for file in $clstr_path/*.fastq; do echo $file; spoa $file > $file.spoa; done
 
-# rename file
-for file in $clstr_path/*.spoa; do mv $file $(echo $file | cut -f1 -d.)_spoa.fasta; done
+# rename file and fasta header
+for file in $clstr_path/*.spoa; do awk 'NR==1{print line">" $0 "|"FILENAME;}END{print $0" "}' $file > $(echo $file | cut -f1 -d.)_spoa.fasta; done
+rm $clstr_path/*.fastq.spoa
