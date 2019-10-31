e6accd8908f1a1909911c89b4ea64af68ab28f8b
Marisa Lim <marisalim@users.noreply.github.com>
2019-10-31 10:26:35 -0400

    clstr table works! subset tally, subset spoa con seqs

--- Pipeline_scripts/clstr_parser.py
+++ Pipeline_scripts/clstr_parser.py
@@ -20,12 +20,14 @@ parser = argparse.ArgumentParser(
     --sampID feather1
     --isocount ./3_readclustering/20190909_minibar100readclstrs/feather1_clstrs/reads_per_cluster.text
     --cdhitclstrs ./3_readclustering/20190909_minibar100readclstrs/feather1_clstrs/cdhit_feather1.fasta.clstr
+    --spoaseqs ./3_readclustering/20190909_minibar100readclstrs/feather1_clstrs/all_clstr_conseqs.fasta
     --output_dir ./3_readclustering/20190909_minibar100readclstrs/feather1_clstrs/'''
 )
 
 parser.add_argument('--sampID', help='Sample ID name', required=True)
 parser.add_argument('--isocount', help='isONclust reads per cluster tally text file', required=True)
 parser.add_argument('--cdhitclstrs', help='cd-hit cluster file', required=True)
+parser.add_argument('--spoaseqs', help='concatenated spoa consensus seq fasta', required=True)
 parser.add_argument('--output_dir', help='Output directory', required=True)
 args=parser.parse_args()
 arg_dict=vars(args)
@@ -33,7 +35,6 @@ arg_dict=vars(args)
 print('Tally of isONclust reads per cluster...')
 isonclstcount = pd.read_csv(str(arg_dict['isocount']), sep=' ', header=None)
 isonclstcount.columns = ['NumReads', 'ClstrID']
-isonclstcount.ClstrID.astype(str)
 print(isonclstcount)
 print('--------------------------------------------------')
 
@@ -63,23 +64,38 @@ for i in cdhit_df.index:
         clstrID = myiso.split('|Consensus...')[j].split('>clstr')[1]
         # print(clstrID
         myls.append(clstrID)
-        myls2.append(mycdhit)
+        myls2.append(mycdhit.split('Cluster_')[1])
 cdhit_df2 = pd.DataFrame(list(zip(myls, myls2)), columns=['ClstrID', 'cdhit'])
-cdhit_df2.ClstrID.astype(str)
+# need to convert dtype from object to numeric in order to merge dfs below
+cdhit_df2['ClstrID'] = pd.to_numeric(cdhit_df2['ClstrID'])
 print(cdhit_df2)
 
-df = pd.merge(isonclstcount, cdhit_df2, on='ClstrID')
+df = isonclstcount.merge(cdhit_df2, on='ClstrID')
 print(df)
+print('--------------------------------------------------')
+print('Great! Now, we will calculate the number of reads that form')
+print('majority cluster by both isONclust & cd-hit.')
+print('By definition, the first row value for cdhit cluster ID, will')
+print('correspond to the isONclust cluster with the greatest number of reads.')
+print('Therefore, we can subset all the rows that match the top row cdhit')
+print('cluster ID and sum over the NumReads column to get total # reads.')
+mycdhitID = df.iloc[0,2]
+subsetdf = df.loc[df['cdhit'] == mycdhitID,]
+print(subsetdf)
+clstrID_ls = subsetdf['ClstrID'].tolist()
+# print(clstrID_ls)
+print('Total num reads for top clusters: ', sum(subsetdf['NumReads']))
 
-----------------------------------------------------------------------
-START HERE -- run below to test this code
-cd /Users/marisalim/Desktop/SPpipe2019/3_readclustering/20190906_qcat100readclstrs/FFPEqleo_clstrs
-python ../../../Pipeline_scripts/clstr_parser.py --sampID FFPEqleo --isocount reads_per_cluster.txt --cdhitclstrs cdhit_FFPEqleo.fasta.clstr --output_dir ./
-
-have this error from the pd.merge():
-ValueError: You are trying to merge on int64 and object columns. If you wish to proceed you should use pd.concat
-----------------------------------------------------------------------
-
-
-
-# maybe use the top spoa consensus sequences and build a master consensus for Medaka?
+print('Now, we can take the spoa consensus sequences for these top cluster(s)')
+print('to create a master consensus sequence for the draft assembly used by Medaka.')
+spoa_conseq = SeqIO.parse(open(str(arg_dict['spoaseqs'])),'fasta')
+with open(str(arg_dict['output_dir']) + 'formedaka.fasta', 'w') as out_file:
+    for fasta in spoa_conseq:
+        name, sequence = fasta.id, str(fasta.seq)
+        # print(name)
+        for clstr in clstrID_ls:
+            fullclstrID = 'clstr' + str(clstr) + '|'
+            # print(fullclstrID)
+            if fullclstrID in name:
+                SeqIO.write(fasta, out_file, 'fasta')
+print('Great! Now, you can run spoa on the forspoaround2.fasta file and feed that to Medaka!')
