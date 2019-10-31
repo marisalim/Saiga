037ca4888d96307e3bb68787b211c300d1d53a3b
Marisa Lim <marisalim@users.noreply.github.com>
2019-10-31 10:25:28 -0400

    changed choice of draft seq
    instead of arbitrarily choosing seq from cd-hit's Cluster_0, now chooses the 1st spoa consensus seq from the cd-hit cluster of isONclust clusters with majority reads/cluster

--- Pipeline_scripts/medaka_corr.sh
+++ Pipeline_scripts/medaka_corr.sh
@@ -7,8 +7,8 @@ input_demult=$1
 input_draft=$2
 output=$3
 
-# edit cd-hit fasta - just use Cluster_0 sequence for draft assembly
-head -n2 $input_draft > $input_draft.cluster0
+# edit parsed clstr fasta and just use 1st sequence for draft assembly
+head -n2 $input_draft > $input_draft.firstclstr
 
 # run Medaka
-medaka_consensus -i $input_demult -d $input_draft.cluster0 -o $output
+medaka_consensus -i $input_demult -d $input_draft.firstclstr -o $output
