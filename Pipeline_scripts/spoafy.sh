7654bce8284293f034cdae148055f902c372feef
Marisa Lim <marisalim@users.noreply.github.com>
2019-11-01 11:12:41 -0400

    run spoa just on subset of iso clsters

--- Pipeline_scripts/spoafy.sh
+++ Pipeline_scripts/spoafy.sh
@@ -2,12 +2,16 @@
 
 source ~/.bashrc
 
-clstr_path=$1
+clstr_file=$1
 
 # make consensus
-for file in $clstr_path/*.fastq; do echo $file; spoa $file > $file.spoa; done
+spoa $clstr_file > $clstr_file.spoa
 
 # rename file and fasta header
-for file in $clstr_path/*.spoa; do awk -v var=">clstr$(echo $file | rev | cut -f1 -d/ |rev |cut -f1 -d.)" 'NR==1{print var "|" $0;}END{print $0" "}' $file > $(echo $file | cut -f1 -d.)_spoa.fasta; done
+awk -v var=">clstr$(echo $clstr_file.spoa | rev | cut -f1 -d/ |rev |cut -f1 -d.)" 'NR==1{print var "|" $0;}END{print $0" "}' $clstr_file.spoa > $(echo $clstr_file.spoa | rev | cut -f3 -d. | rev)_spoa.fasta
 
-rm $clstr_path/*.fastq.spoa
+rm $clstr_file.spoa
+
+# old - use if you want to run spoa on all fastq files within a $clstr_path directory
+# for file in $clstr_path/*.fastq; do echo $file; spoa $file > $file.spoa; done
+# for file in $clstr_path/*.spoa; do awk -v var=">clstr$(echo $file | rev | cut -f1 -d/ |rev |cut -f1 -d.)" 'NR==1{print var "|" $0;}END{print $0" "}' $file > $(echo $file | cut -f1 -d.)_spoa.fasta; done
