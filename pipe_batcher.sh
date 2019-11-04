5d069de01bd39cd5e6afc5dcc79e807353fe445b
Marisa Lim <marisalim@users.noreply.github.com>
2019-11-04 08:39:59 -0500

    additional perthresh flag

--- pipe_batcher.sh
+++ pipe_batcher.sh
@@ -4,51 +4,51 @@
 
 ## need to run each dataset 1x for the raw read nanoplots
 ## --demult, --samps, --db flags won't be used here, but have to include them since I made them required
-python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190913_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190913 --demult qcat --samps 20190913_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
 ## 1x for each qcat and minibar + filtering + demult nanoplots (full dat)
 ## --db flag won't be used here, but have to include it since I made it required
-# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset none --db Sept2019_Sanger_cytb.fasta
-
-# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190913_sample_list.txt --subset none --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190913 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+#
+# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190913 --demult qcat --samps 20190913_sample_list.txt --subset none --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
 ## then the subset versions starting from build subsets through stats parsing
 ## this set finally uses all the flags!
-# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
+# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
-# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
-# python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190906 --demult minibar --samps 20190906_sample_list.txt --mbseqs 20190906_primerindex.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult minibar --samps 20190909_sample_list.txt --mbseqs 20190909_primerindex.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult minibar --samps 20190911_sample_list.txt --mbseqs 20190911_primerindex.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult minibar --samps 20190913_sample_list.txt --mbseqs 20190913_primerindex.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
-# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190913_sample_list.txt --subset 100 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult qcat --samps 20190913_sample_list.txt --subset 100 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
-# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190913_sample_list.txt --subset 500 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult qcat --samps 20190913_sample_list.txt --subset 500 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
 
-# python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
-# python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190913_sample_list.txt --subset 5000 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190906 --demult qcat --samps 20190906_sample_list.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190909 --demult qcat --samps 20190909_sample_list.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190911 --demult qcat --samps 20190911_sample_list.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
+python devo_wrapper.py --datID 20190913 --demult qcat --samps 20190913_sample_list.txt --subset 5000 --perthresh 0.1 --db Sept2019_Sanger_cytb.fasta
