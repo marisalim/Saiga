4d5b3d8a5fea753d036d66212b735f5ec552952e
Marisa Lim <marisalim@users.noreply.github.com>
2019-11-14 14:21:40 -0500

    made this user input

--- Pipeline_scripts/revcomp_check.sh
+++ Pipeline_scripts/revcomp_check.sh
@@ -4,6 +4,7 @@ source ~/.bashrc
 
 clstr_file=$1
 cdhit_out=$2
+thresh=$3
 
 # note, -c must be >= 0.8
-cd-hit-est -i $clstr_file -o $cdhit_out -c 0.8
+cd-hit-est -i $clstr_file -o $cdhit_out -c $thresh
