dcfd440e29c4aa2d254b6fe1888375fb9235b69f
Marisa Lim <marisalim@users.noreply.github.com>
2019-11-22 14:53:32 -0500

    added demo version

--- pipe_batcher.sh
+++ pipe_batcher.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# testing out new flags
-# allows you to run all in one command
-python devo_wrapper.py --dat 20190906 --samps 20190906_sample_list.txt --rawNP n --demultgo n --filt n --subgo y --clust y --demult qcat --qcat_minscore 99 --ONTbarcodekit PBC001 --qs 7 --buffer 100 --subset 10 --perthresh 0.1 --cdhitsim 0.8 --db Sept2019_Sanger_cytb.fasta
-python devo_wrapper.py --dat 20190906 --samps 20190906_sample_list.txt --rawNP n --demultgo n --filt n --subgo y --clust y --demult minibar --mbseqs 20190906_primerindex.txt --mb_idx_dist 2 --mb_pr_dist 11 --qs 7 --buffer 100 --subset 10 --perthresh 0.1 --cdhitsim 0.8 --db Sept2019_Sanger_cytb.fasta
+## DEMO version
+python devo_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP y --demultgo y --filt y --subgo y --clust y --demult qcat --qcat_minscore 99 --ONTbarcodekit PBC001 --qs 7 --buffer 100 --subset 50 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
+python devo_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP y --demultgo y --filt y --subgo y --clust y --demult minibar --mbseqs demo_primerindex.txt --mb_idx_dist 2 --mb_pr_dist 11 --qs 7 --buffer 100 --subset 50 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
 
 ## batcher cmds for all the datasets/demultiplex trials/subsets for MinION manuscript
 ## 1. Raw read NanoPlots
