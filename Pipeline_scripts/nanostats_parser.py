f95dad5467072e86c5f2bcfa0a7915683e5cb268
Marisa Lim <marisalim@users.noreply.github.com>
2019-12-05 12:22:57 -0500

    NPs for demult & filt full dats

--- Pipeline_scripts/nanostats_parser.py
+++ Pipeline_scripts/nanostats_parser.py
@@ -21,6 +21,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('--dat', help='dataset ID')
 parser.add_argument('--demult', help='the demultiplexing software used')
+parser.add_argument('--filtstat', help='filtered or not. Options: y, n', required=True)
 parser.add_argument('--npdir', help='Directory path with NanoStats.txt files', required=True)
 parser.add_argument('--output_dir', help='Output directory', required=True)
 args=parser.parse_args()
@@ -28,20 +29,40 @@ arg_dict=vars(args)
 
 # extract the top lines with stats
 # formulate into table
-nanostat_list = []
-for nanostat in Path(arg_dict['npdir']).rglob('*_filtered_demNanoStats.txt'):
-    # print(nanostat)
-    thesamp = str(nanostat).split('_filtered_demNanoStats.txt')[0].split('/')[-1]
-    # print(thesamp)
-    thedemult = str(nanostat).split('_filtered_demNanoStats.txt')[0].split('_demultiplexouts')[0].split('_')[-1]
-    print(thedemult)
-    df = pd.read_csv(nanostat, sep=':').iloc[:7,]
-    df.columns=['GeneralSummary', 'Value']
-    df2 = df.set_index('GeneralSummary').transpose()
-    df2['SampID'] = thesamp
-    df2['Demult'] = thedemult
-    # print(df2)
-    nanostat_list.append(df2)
-nanostat_concat = pd.concat(nanostat_list, axis=0, sort=False)
-print(nanostat_concat)
-nanostat_concat.to_csv(arg_dict['output_dir']+arg_dict['dat']+'_'+arg_dict['demult']+'_allsamps_nanostats.txt', sep='\t', index=False)
+
+if arg_dict['filtstat'] == 'y':
+    nanostat_list = []
+    for nanostat in Path(arg_dict['npdir']).rglob('*_filtered_demNanoStats.txt'):
+        # print(nanostat)
+        thesamp = str(nanostat).split('_filtered_demNanoStats.txt')[0].split('/')[-1]
+        # print(thesamp)
+        thedemult = str(nanostat).split('_filtered_demNanoStats.txt')[0].split('_demultiplexouts')[0].split('_')[-1]
+        print(thedemult)
+        df = pd.read_csv(nanostat, sep=':').iloc[:7,]
+        df.columns=['GeneralSummary', 'Value']
+        df2 = df.set_index('GeneralSummary').transpose()
+        df2['SampID'] = thesamp
+        df2['Demult'] = thedemult
+        # print(df2)
+        nanostat_list.append(df2)
+    nanostat_concat = pd.concat(nanostat_list, axis=0, sort=False)
+    print(nanostat_concat)
+    nanostat_concat.to_csv(arg_dict['output_dir']+arg_dict['dat']+'_'+arg_dict['demult']+'_allsamps_filtnanostats.txt', sep='\t', index=False)
+elif arg_dict['filtstat'] == 'n':
+    nanostat_list = []
+    for nanostat in Path(arg_dict['npdir']).rglob('*_demNanoStats.txt'):
+        # print(nanostat)
+        thesamp = str(nanostat).split('_demult_demNanoStats.txt')[0].split('/')[-1]
+        # print(thesamp)
+        thedemult = str(nanostat).split('_demult_demNanoStats.txt')[0].split('_demultiplexouts')[0].split('_')[-1]
+        print(thedemult)
+        df = pd.read_csv(nanostat, sep=':').iloc[:7,]
+        df.columns=['GeneralSummary', 'Value']
+        df2 = df.set_index('GeneralSummary').transpose()
+        df2['SampID'] = thesamp
+        df2['Demult'] = thedemult
+        # print(df2)
+        nanostat_list.append(df2)
+    nanostat_concat = pd.concat(nanostat_list, axis=0, sort=False)
+    print(nanostat_concat)
+    nanostat_concat.to_csv(arg_dict['output_dir']+arg_dict['dat']+'_'+arg_dict['demult']+'_allsamps_demultnanostats.txt', sep='\t', index=False)
