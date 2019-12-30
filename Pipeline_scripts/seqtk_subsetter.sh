0da3e1337fb7e5205a5bec8875632744dc3b6376
Marisa Lim <marisalim@users.noreply.github.com>
2019-12-30 09:55:05 -0800

    adding seed as an opt

--- Pipeline_scripts/seqtk_subsetter.sh
+++ Pipeline_scripts/seqtk_subsetter.sh
@@ -5,6 +5,7 @@
 # generate data subsets
 # ------------------------------------------------------------
 
+
 source ~/.bashrc
 
 # use seqtk to create random subsets of fastq files
@@ -12,8 +13,9 @@ mysub=$1
 mysampfq=$2 #just prefix, no extension
 myoutput_dir=$3
 demultpath=$4
+seed=$5
 
 if [ ! -d '$myoutput_dir' ]; then mkdir $myoutput_dir; fi
 
 # -s is random seed
-seqtk sample -s100 $demultpath/$mysampfq.fastq $mysub > $myoutput_dir/$mysampfq$mysub.fastq
+seqtk sample -s$seed $demultpath/$mysampfq.fastq $mysub > $myoutput_dir/$mysampfq$mysub.$seed.fastq
