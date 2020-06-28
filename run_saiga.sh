f23175b1224d4dd30549932327a22163b92e5462
Marisa Lim <marisalim@users.noreply.github.com>
2020-06-28 14:37:23 -0700

    reset demo flag opts to y

--- run_saiga.sh
+++ run_saiga.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ## DEMO version
-python saiga_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP n --demultgo n --filt n --subgo n --clust y --demult qcat --qcat_minscore 99 --ONTbarcodekit PBC001 --qs 7 --buffer 100 --subset none --subseed 100 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
-#python saiga_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP n --demultgo n --filt n --subgo y --clust y --demult minibar --mbseqs demo_primerindex.txt --mb_idx_dist 2 --mb_pr_dist 11 --qs 7 --buffer 100 --subset none --subseed 100 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
+python saiga_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP y --demultgo y --filt y --subgo y --clust y --demult qcat --qcat_minscore 99 --ONTbarcodekit PBC001 --qs 7 --buffer 100 --subset 50 --subseed 100 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
+#python saiga_wrapper.py --dat demo --samps demo_sample_list.txt --rawNP y --demultgo y --filt y --subgo y --clust y --demult minibar --mbseqs demo_primerindex.txt --mb_idx_dist 2 --mb_pr_dist 11 --qs 7 --buffer 100 --subset 50 --subseed 100 --perthresh 0.1 --cdhitsim 0.8 --db demo.fasta
 
